c264e386e10a9f4352dd7866754d811fb116d38c
Chris Joakim <christopher.joakim@gmail.com>
2020-11-03 15:55:21 -0500

    updated python device shell script

--- py_device1/device_send_messages.sh
+++ py_device1/device_send_messages.sh
@@ -2,9 +2,9 @@
 
 # Usage:
 #   $ ./device_send_messages.sh <count> <sleep_milliseconds>
-#   $ ./device_send_messages.sh 10 3.33
+#   $ ./device_send_messages.sh 10 1000
 #
-# Chris Joakim, Microsoft, 2020/10/26
+# Chris Joakim, Microsoft, 2020/11/03
 
 msg_count=$1
 sleep_milliseconds=$2
