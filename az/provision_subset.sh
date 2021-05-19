da2796dd7c65e81d6f459ac5e3fe765dd698af36
Chris Joakim <christopher.joakim@gmail.com>
2021-05-19 12:30:49 -0400

    added az/provision_subset.sh

---
+++ az/provision_subset.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Bash script with AZ CLI to automate the creation of the resources for this project.
+# This is a subset of "provision_all.sh" in this directory, simply create a
+# CosmosDB/SQL, Synapse, Synapse Spark Pool
+# (No IoT Hub and devices, Stream Analytics, SQL Pool)
+#
+# Chris Joakim, Microsoft, 2021/05/19
+#
+# See https://docs.microsoft.com/en-us/cli/azure/?view=azure-cli-latest
+
+az login
+az account set -s $AZURE_SUBSCRIPTION_ID
+az account show 
+
+source ./provisioning_env.sh
+sleep_seconds=60
+
+./extensions.sh install
+./extensions.sh list
+
+./cosmos_sql.sh create
+sleep $sleep_seconds
+./cosmos_sql.sh info
+
+# Omit this
+# ./iothub.sh create
+# sleep $sleep_seconds
+# ./iothub.sh info
+
+# Omit this
+# ./stream_analytics.sh create
+# sleep $sleep_seconds
+
+./synapse.sh create 
+sleep $sleep_seconds
+./synapse.sh info
+sleep $sleep_seconds
+
+./synapse.sh create_spark_pool
+sleep $sleep_seconds
+./synapse.sh info
+sleep $sleep_seconds
+
+# Omit this
+# ./synapse.sh create_sql_pool
+# sleep $sleep_seconds
+
+./synapse.sh info
+
+# Omit this
+# Then, run these steps manually:
+# Set environment variable AZURE_IOTHUB_CONN_STR
+# ./iothub.sh link_dps_to_hub
+# ./iothub.sh info
+
+# Omit this
+# Then, register two devices:
+# ./iothub.sh register_device1
+# ./iothub.sh register_device2
+# Set AZURE_IOTHUB_DEVICE1_CONN_STR per connection string value in Azure Portal
+# Set AZURE_IOTHUB_DEVICE2_CONN_STR per connection string value in Azure Portal
+
+echo 'done'
